0e6b564944392af1ed17f2525a6274d29fbcb394
Arthur <129552454+arthurbnhm@users.noreply.github.com>
2023-06-06 14:35:55 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,12 @@
-window.function = function () {
-  var url = "https://www.gold.fr/get-gs-price/?id=gold&format=json";
-  var response = fetch(url).then(response => response.json());
-  return {
-    value_eur: response.value_eur,
-    value_dol: response.value_dol
+window.function = async function () {
+  var url = "https://www.gold.fr/get-gs-price/?id=gold&format=json"; 
+  var response = await fetch(url);
+  var data = await response.json();
+  return { 
+    value_eur: data.value_eur, 
+    value_dol: data.value_dol 
   };
 }
+
+
+
